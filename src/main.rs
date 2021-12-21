5939d00eb54df7449fa3cb2e027b284eb6c10971
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2021-12-20 16:57:46 -0800

    user &str

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ extern crate pest_derive;
 
 // use std::fmt::Result;
 
+use std::collections::HashMap;
 use std::fmt::Debug;
+use std::hash::Hash;
 
 use pest::iterators::Pair;
 
@@ -77,106 +79,170 @@ struct Saty {
     final_expr: Expr,
 }
 
-fn make_expr(item: Pair<Rule>) -> Result<Expr, String> {
-
-    let rule = item.as_rule();
-    let text = item.as_str();
-
-    let contents: Vec<_> = item.into_inner().collect();
-
-    match rule {
-        Rule::literal => {
-            let value = contents[0].as_str() == "TRUE";
-            Ok(Expr::Literal(value))
-        }
-        Rule::variable => {
-            Ok(Expr::Var(text.to_string()))
-        }
-        Rule::apply_prefix => {
-            let op = contents[0].as_str();
-            let operand = contents[1].to_owned();
-            // let expr = contents[1].as_str();
-            let expr = make_expr(operand)?;
-            Ok(Expr::Prefix(op.to_string(), Box::new(expr)))
-        }
-        Rule::apply_infix => {
-            let lhs = contents[0].to_owned();
-            let op = contents[1].as_str();
-            let rhs = contents[2].to_owned();
-            let lhs_expr = make_expr(lhs)?;
-            let rhs_expr = make_expr(rhs)?;
-            Ok(Expr::Infix(op.to_string(), Box::new(lhs_expr), Box::new(rhs_expr)))
-        }
-        Rule::list => {
-            let mut exprs = Vec::new();
-            for item in contents {
-                let expr = make_expr(item)?;
-                exprs.push(expr);
+impl Expr {
+    fn eval(&self, variables: &HashMap<&str, bool>) -> bool {
+        match self {
+            Expr::Literal(b) => *b,
+            Expr::Var(name) => *variables.get(name.as_str()).unwrap(),
+            Expr::Prefix(op, expr) =>
+                match expr.as_ref() {
+                    Expr::List(exprs) => {
+                        let bs = exprs.iter().map(|e| e.eval(variables)).collect();
+                        match op.as_str() {
+                            "ALL" => Ops::all(bs),
+                            "ANY" => Ops::any(bs),
+                            "NONE" => Ops::none(bs),
+                            "ONE" => Ops::one(bs),
+                            _ => panic!("Unknown prefix operator for type list: {}", op),
+                        }
+                    },
+                    _ => {
+                        match op.as_str() {
+                            "NOT" => Ops::not(expr.eval(variables)),
+                            _ => panic!("unknown prefix operator for type bool: {}", op),
+                        }
+                    }
+                    
+                    
+                    
+                    
+            },
+            Expr::Infix(op, lhs, rhs) => match op.as_str() {
+                "AND" => Ops::and(lhs.eval(variables), rhs.eval(variables)),
+                "OR" => Ops::or(lhs.eval(variables), rhs.eval(variables)),
+                "XOR" => Ops::xor(lhs.eval(variables), rhs.eval(variables)),
+                "NAND" => Ops::nand(lhs.eval(variables), rhs.eval(variables)),
+                "NOR" => Ops::nor(lhs.eval(variables), rhs.eval(variables)),
+                "XNOR" => Ops::xnor(lhs.eval(variables), rhs.eval(variables)),
+                _ => panic!("unknown infix operator: {}", op),
+            },
+            _ => {
+                panic!("unknown evaluation for expression type: {:?}", self);
             }
-            Ok(Expr::List(exprs))
         }
-        _ => {
-            Err(format!("Unknown rule: {:?} {}", rule, text))
+    }
+
+    fn new(item: Pair<Rule>) -> Result<Expr, String> {
+
+        let rule = item.as_rule();
+        let text = item.as_str();
+    
+        let contents: Vec<_> = item.into_inner().collect();
+    
+        match rule {
+            Rule::literal => {
+                let value = contents[0].as_str() == "TRUE";
+                Ok(Expr::Literal(value))
+            }
+            Rule::variable => {
+                Ok(Expr::Var(text.to_string()))
+            }
+            Rule::apply_prefix => {
+                let op = contents[0].as_str();
+                let operand = contents[1].to_owned();
+                // let expr = contents[1].as_str();
+                let expr = Expr::new(operand)?;
+                Ok(Expr::Prefix(op.to_string(), Box::new(expr)))
+            }
+            Rule::apply_infix => {
+                let lhs = contents[0].to_owned();
+                let op = contents[1].as_str();
+                let rhs = contents[2].to_owned();
+                let lhs_expr = Expr::new(lhs)?;
+                let rhs_expr = Expr::new(rhs)?;
+                Ok(Expr::Infix(op.to_string(), Box::new(lhs_expr), Box::new(rhs_expr)))
+            }
+            Rule::list => {
+                let mut exprs = Vec::new();
+                for item in contents {
+                    let expr = Expr::new(item)?;
+                    exprs.push(expr);
+                }
+                Ok(Expr::List(exprs))
+            }
+            _ => {
+                Err(format!("Unknown rule: {:?} {}", rule, text))
+            }
         }
     }
 }
 
-fn make_assignment(item: Pair<Rule>) -> Result<Assignment, String> {
-    // let rule = item.as_rule();
-    let text = item.as_str();
+impl Saty {
 
-    let contents: Vec<_> = item.into_inner().collect();
-    if contents.len() != 2 {
-        Err(format!("Malformed assignment statement {}", text))
-    }
-    else {
-        let name_pair = contents[0].to_owned();
-        let expr_pair = contents[1].to_owned();
-        if name_pair.as_rule() != Rule::variable {
-            Err(format!("Malformed assignment statement {}", text))
+    fn eval<'a>(&self, input_variables: HashMap<&'a str, bool>) -> bool {
+        let mut assigned_values = input_variables.clone();
+        for Assignment { name, expr } in self.assignments.iter() {
+            let value = expr.eval(&assigned_values);
+            assigned_values.insert(name.as_str(), value);
         }
-        // else if expr_pair.as_rule() != Rule::expr {
-        //     Err(format!("Malformed assignment statement {}", text))
-        // }
+        self.final_expr.eval(&assigned_values)
+    }
+
+    fn new<'a >(text: impl ToString, allowed_variables: impl Into<Option<Vec<&'a str>>>) -> Result<Saty, String> {
+        // item.as_rule()
+        
+        // let text = item.as_str();
+        let text_string = text.to_string();
+        let text_str = text_string.as_str();
+        let mut pairs = SatyParser::parse(
+            Rule::file, 
+            text_str).ok().ok_or("Failed to parse saty")?;
+    
+        let pair = pairs.next().ok_or("Input was empty")?;
+    
+        let rule = pair.as_rule();
+    
+        let contents: Vec<_> = pair.into_inner().collect();
+    
+        if contents.len() == 1 {
+            return Err("Empty input cannot be converted to Saty".to_string());
+        }
+        else if rule != Rule::file {
+            Err(format!("Expected rule, got {:?}", rule))
+        }
         else {
-            let name = name_pair.as_str();
-            let expr = make_expr(expr_pair)?;
-            Ok(Assignment {
-                name: name.to_string(),
-                expr: expr,
+            let n_assigns = contents.len() - 2;
+            let assign_pairs = contents.iter().cloned().take(n_assigns);
+            let assignments = assign_pairs.map(Assignment::new).collect::<Result<Vec<Assignment>, String>>()?;
+            let final_pair = contents.iter().skip(n_assigns).next().ok_or("No final expression")?;
+            Ok(Saty {
+                assignments,
+                final_expr: Expr::new(final_pair.to_owned())?,
             })
         }
     }
 }
 
-fn make_saty(item: Pair<Rule>) -> Result<Saty, String> {
-    // item.as_rule()
-    let rule = item.as_rule();
-    // let text = item.as_str();
+impl Assignment {
+    fn new(item: Pair<Rule>) -> Result<Assignment, String> {
+        // let rule = item.as_rule();
+        let text = item.as_str();
 
-    let contents: Vec<_> = item.into_inner().collect();
-
-    // for inner in contents.iter() {
-    //     println!("{:?}", inner);
-    // }
-
-    if contents.len() == 1 {
-        return Err("Empty input cannot be converted to Saty".to_string());
-    }
-    else if rule != Rule::file {
-        Err(format!("Expected rule, got {:?}", rule))
-    }
-    else {
-        let n_assigns = contents.len() - 2;
-        let assign_pairs = contents.iter().cloned().take(n_assigns);
-        let assignments = assign_pairs.map(make_assignment).collect::<Result<Vec<Assignment>, String>>()?;
-        let final_pair = contents.iter().skip(n_assigns).next().ok_or("No final expression")?;
-        Ok(Saty {
-            assignments,
-            final_expr: make_expr(final_pair.to_owned())?,
-        })
+        let contents: Vec<_> = item.into_inner().collect();
+        if contents.len() != 2 {
+            Err(format!("Malformed assignment statement {}", text))
+        }
+        else {
+            let name_pair = contents[0].to_owned();
+            let expr_pair = contents[1].to_owned();
+            if name_pair.as_rule() != Rule::variable {
+                Err(format!("Malformed assignment statement {}", text))
+            }
+            // else if expr_pair.as_rule() != Rule::expr {
+            //     Err(format!("Malformed assignment statement {}", text))
+            // }
+            else {
+                let name = name_pair.as_str();
+                let expr = Expr::new(expr_pair)?;
+                Ok(Assignment {
+                    name: name.to_string(),
+                    expr: expr,
+                })
+            }
+        }
     }
 }
+
 
 impl Debug for Assignment {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -219,15 +285,23 @@ impl Debug for Saty {
 }
 
 fn main() {
+
+    let allowed_variables = vec!["orange", "peach", "pink", "red", "green", "blue", "yellow"];
+
     let input = "(NOT ANY [ orange peach (pink) ]) OR red AND NOT green";
     println!("{}", input);
 
-    // pest parses the input
-    let successful_parse =
-        SatyParser::parse(
-            Rule::file, 
-            input).expect("failed to parse input").next().unwrap();
+    // parse result into an AST
+    let saty = Saty::new(input, allowed_variables).unwrap();
+    println!("{:?}", saty);
 
-    // turn the parse result into an AST
-    println!("{:?}", make_saty(successful_parse));
+    let mut input_variables = HashMap::new();
+    input_variables.insert("orange", true);
+    input_variables.insert("peach", true);
+    input_variables.insert("pink", false);
+    input_variables.insert("red", true);
+    input_variables.insert("green", false);
+
+    let result = saty.eval(input_variables);
+    println!("{:?}", result);
 }
